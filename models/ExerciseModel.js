4252c01add853ef930b654c5af72361a6ed73daf
nicknack <nicknicknick22@aol.com>
2020-03-25 15:19:36 -0400

    Initial commit & added HTML routes

---
+++ models/ExerciseModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    type: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
